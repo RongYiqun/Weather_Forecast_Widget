2e87e28dfb897d77c1a69e19ac86025760c34da9
Yiqun Rong <411640583@qq.com>
2021-01-16 14:17:54 +1100

    add functions to call api

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// CORS Proxies  https://api.allorigins.win
+
+export function searchLocation(location) {
+  let url;
+  if (typeof location === "string") {
+    url = `https://www.metaweather.com/api/location/search/?query=${location}`;
+  } else {
+    url = `https://www.metaweather.com/api/location/search/?lattlong=${location.latitude},${location.longitude}`;
+  }
+  return axios
+    .get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+    .then((res) => res.data.contents);
+}
+
+export function getWeathersByWoeid(woeid) {
+  const url = `https://www.metaweather.com/api/location/${woeid}/`;
+  return axios
+    .get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+    .then((res) => res.data.contents);
+}
